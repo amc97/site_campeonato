c37389a856f5fb5493f17ec6326b1e652bbab1f2
Anderson Malheiro <malheiroanderson97@gmail.com>
2017-04-30 14:11:52 -0300

    Mudança na configuração do acesso ao banco

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -29,8 +29,8 @@
  * These are the database login details
  */
 define("HOST", "localhost"); 			// The host you want to connect to. 
-define("USER", "admin"); 			// The database username. 
-define("PASSWORD", "amc9799"); 	// The database password. 
+define("USER", "root"); 			// The database username. 
+define("PASSWORD", ""); 	// The database password. 
 define("DATABASE", "secure_login");             // The database name.
 
 /**
