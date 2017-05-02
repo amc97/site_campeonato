89cd75bf586dbc24b438fba1d0292d4e606ebf47
Gabriel Lima <gabriel.lima.rds@gmail.com>
2017-05-02 12:54:22 -0300

    Mudança do layout do Painel de Controle

--- controlPanel.php
+++ controlPanel.php
@@ -1,20 +1,4 @@
 <?php
-/**
- * Copyright (C) 2013 peredur.net
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 include_once 'includes/db_connect.php';
 include_once 'includes/functions.php';
 
@@ -22,107 +6,142 @@ sec_session_start();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Painel de controle</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">    
-        <link href="css/simple-sidebar.css" rel="stylesheet">
-    </head>
-    <body>
-        <?php if (login_check($mysqli) == true) : ?>                
-                <script>
-                    function visualizarForm(opcao) {       
-                        if(opcao == '0') {                          
-                            document.getElementById('0').style.display = 'block';
-                            document.getElementById('1').style.display = 'none';
-                            document.getElementById('2').style.display = 'none';
-                            document.getElementById('3').style.display = 'none';
-                        }
-                        if(opcao == '1') {                          
-                            document.getElementById('0').style.display = 'none';
-                            document.getElementById('1').style.display = 'block';
-                            document.getElementById('2').style.display = 'none';
-                            document.getElementById('3').style.display = 'none';
-                        }
-                        if(opcao == '2') {          
-                            document.getElementById('0').style.display = 'none';
-                            document.getElementById('1').style.display = 'none';
-                            document.getElementById('2').style.display = 'block';
-                            document.getElementById('3').style.display = 'none';       
-                        }
-                        if(opcao == '3') {          
-                            document.getElementById('0').style.display = 'none';
-                            document.getElementById('1').style.display = 'none';
-                            document.getElementById('2').style.display = 'none';
-                            document.getElementById('3').style.display = 'block';       
-                        }
-                    }
 
-                    function logout(){
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                    }
-            </script>
+    <title>Painel de Controle</title>
 
-            <?php
-                function imprimeForms(){
-                    for($i = 1; $i <= 12; $i++){
-                        echo "<div class='row'>";
-                        if($i < 10){
-                            echo "0$i - ";
-                        }else{
-                            echo "$i - ";
-                        }                
-                        echo "<div class='form-group'>";
-                        echo "<input type='text' class='form-control' placeholder='Nome'>";
-                        echo "</div>";
-                        echo "<div class='form-group'>";
-                        echo "<input type='date' class='form-control'>";
-                        echo "</div>";
-                        echo "<div class='form-group'>";
-                        echo "<input type='text' class='form-control' placeholder='CPF'>";
-                        echo "</div>";
-                        echo "</div>";
-                        echo "<br>";
-                    }
-                    echo "<br>";
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+	<?php if (login_check($mysqli) == true) : ?>
+		<script>
+            function visualizarForm(opcao) {       
+                if(opcao == '0') {                          
+                    document.getElementById('0').style.display = 'block';
+                    document.getElementById('1').style.display = 'none';
+                    document.getElementById('2').style.display = 'none';
+                    document.getElementById('3').style.display = 'none';
+                }
+                if(opcao == '1') {                          
+                    document.getElementById('0').style.display = 'none';
+                    document.getElementById('1').style.display = 'block';
+                    document.getElementById('2').style.display = 'none';
+                    document.getElementById('3').style.display = 'none';
+                }
+                if(opcao == '2') {          
+                    document.getElementById('0').style.display = 'none';
+                    document.getElementById('1').style.display = 'none';
+                    document.getElementById('2').style.display = 'block';
+                    document.getElementById('3').style.display = 'none';       
+                }
+                if(opcao == '3') {          
+                    document.getElementById('0').style.display = 'none';
+                    document.getElementById('1').style.display = 'none';
+                    document.getElementById('2').style.display = 'none';
+                    document.getElementById('3').style.display = 'block';       
+                }
+            }
+
+            function logout(){
+
+            }
+        </script>
+
+        <?php
+            function imprimeForms(){
+                for($i = 1; $i <= 12; $i++){
+                    echo "<div class='row'>";
+                    if($i < 10){
+                        echo "0$i - ";
+                    }else{
+                        echo "$i - ";
+                    }                
                     echo "<div class='form-group'>";
-                    echo "<input type='submit' class='btn btn-info center-block'>";
+                    echo "<input type='text' class='form-control' placeholder='Nome'>";
                     echo "</div>";
+                    echo "<div class='form-group'>";
+                    echo "<input type='date' class='form-control'>";
+                    echo "</div>";
+                    echo "<div class='form-group'>";
+                    echo "<input type='text' class='form-control' placeholder='CPF'>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "<br>";
                 }
+                echo "<br>";
+                echo "<div class='form-group'>";
+                echo "<input type='submit' class='btn btn-info center-block'>";
+                echo "</div>";
+            }
 
-                function imprimeTimes(){
-                    for($i = 1; $i <= 20; $i++){
-                        echo"<option>Time $i</option>";
-                    }
+            function imprimeTimes(){
+                for($i = 1; $i <= 20; $i++){
+                    echo"<option>Time $i</option>";
                 }
+            }
 
-            ?>
+        ?>
 
-            <div id="wrapper" class="toggled">
+	    <div id="wrapper">
 
-                <!-- Sidebar -->
-                <div id="sidebar-wrapper">
-                    <ul class="sidebar-nav">
-                        <li class="sidebar-brand">
-                            <a onClick="visualizarForm('0');" href="#">Painel de Controle</a>
-                        </li>
-                        <li>
-                            <a onClick="visualizarForm('1');" href="#">Cadastrar time</a>
-                        </li>
-                        <li>
-                            <a onClick="visualizarForm('2');" href="#">Cadastrar jogadores</a>
-                        </li>
-                        <li>
-                            <a onClick="visualizarForm('3');" href="#">Atualizar dados</a>
-                        </li>                
-                        <li>
-                            <a href="includes/logout.php">Sair</a>
-                        </li>           
-                    </ul>
-                </div>
-                <!-- /#sidebar-wrapper -->
-                
-                <div id="0" style="display:block;">
+	        <!-- Navigation -->
+	        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	            <!-- Brand and toggle get grouped for better mobile display -->
+	            <div class="navbar-header">
+	                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+	                    <span class="sr-only">Toggle navigation</span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                </button>
+	                <a onClick="visualizarForm('0');" class="navbar-brand" href="controlPanel.php">PAINEL DE CONTROLE</a>
+	            </div>
+	            <!-- Top Menu Items -->
+	            <ul class="nav navbar-right top-nav">
+	                
+	                <li class="dropdown">
+	                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo htmlentities($_SESSION['username']); ?> <b class="caret"></b></a>
+	                    <ul class="dropdown-menu">
+	                        
+	                        <li>
+	                            <a href="includes/logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+	                        </li>
+	                    </ul>
+	                </li>
+	            </ul>
+	            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+	            <div class="collapse navbar-collapse navbar-ex1-collapse">
+	                <ul class="nav navbar-nav side-nav">
+	                    <li>
+	                        <a onClick="visualizarForm('1');" href="#"><i class="fa fa-fw fa-dashboard"></i> Cadastrar Time</a>
+	                    </li>
+	                    <li>
+	                        <a onClick="visualizarForm('2');" href="#"><i class="fa fa-fw fa-bar-chart-o"></i> Cadastrar Jogadores</a>
+	                    </li>
+	                    <li>
+	                        <a onClick="visualizarForm('3');" href="#"><i class="fa fa-fw fa-table"></i> Atualizar Dados</a>
+	                    </li>
+	                    
+	                </ul>
+	            </div>
+	            <!-- /.navbar-collapse -->
+	        </nav>
+
+	        <div id="page-wrapper">
+	        	<div id="0" style="display:block;">
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-lg-6 col-lg-offset-3 text-center" style="padding-top: 10%; padding-botton: 10%">
@@ -201,27 +220,42 @@ sec_session_start();
                             </div>
                         </div>
                     </div>
-                </div><!-- /#page-content-wrapper -->
+                </div>	            
 
-            </div>
-            <!-- /#wrapper -->
+	        </div>
+	        <!-- /#page-wrapper -->
 
-            <script type="text/javascript">
-                    document.getElementById("files").onchange = function () {
-                        var reader = new FileReader();
+	    </div>
+	    <!-- /#wrapper -->
 
-                        reader.onload = function (e) {
-                        // get loaded data and render thumbnail.
-                        document.getElementById("image").src = e.target.result;		                        
-                      };
-                        // read the image file as a data URL.
-                        reader.readAsDataURL(this.files[0]);
-                    };
-            </script>
-        <?php else : ?>
-            <p>
-                <span class="error">Você não está autorizado a acessar essa página.</span> Por favor faça <a href="index.php">login</a>.
-            </p>
-        <?php endif; ?>
-    </body>
+	    <!-- jQuery -->
+	    <script src="js/jquery.js"></script>
+
+	    <!-- Bootstrap Core JavaScript -->
+	    <script src="js/bootstrap.min.js"></script>
+
+	    <!-- Morris Charts JavaScript -->
+	    <script src="js/plugins/morris/raphael.min.js"></script>
+	    <script src="js/plugins/morris/morris.min.js"></script>
+	    <script src="js/plugins/morris/morris-data.js"></script>
+
+	    <script type="text/javascript">
+            document.getElementById("files").onchange = function () {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                // get loaded data and render thumbnail.
+                document.getElementById("image").src = e.target.result;		                        
+              };
+                // read the image file as a data URL.
+                reader.readAsDataURL(this.files[0]);
+            };
+        </script>
+	<?php else : ?>
+        <p>
+        	<span class="error">Você não está autorizado a acessar essa página.</span> Por favor faça <a href="index.php">login</a>.
+        </p>
+   	<?php endif; ?>
+</body>
+
 </html>
